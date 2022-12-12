2da2a20cc5fc9b95cb6b54818f1de8b1047147ff
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2022-12-12 01:35:10 +0100

    Friendly

--- Core/Modules/Logs/LogsConfig.cs
+++ Core/Modules/Logs/LogsConfig.cs
@@ -10,10 +10,10 @@ public class LogsConfig : IConfig
 
     public Dictionary<WebhookType, string> Webhooks { get; set; } = new()
     {
-        [WebhookType.CommandLogs] = "https://discord.com/api/webhooks/1013504100060647504/gqHwm_G0X82GshDGO4hUZWSdexpHIkQgkgP461msUD6iS1nfjyXkV_7ptCjsetERMep5",
-        [WebhookType.GameLogs] = "https://discord.com/api/webhooks/1013504011946696845/QP8xKwevpogqXrHipDcWCxzOFXSbrN2cQzNO7MvFc-Rf7fhSv-jOZi1vMmGEc_myWUmA",
-        [WebhookType.KillLogs] = "https://discord.com/api/webhooks/1013504011946696845/QP8xKwevpogqXrHipDcWCxzOFXSbrN2cQzNO7MvFc-Rf7fhSv-jOZi1vMmGEc_myWUmA",
-        [WebhookType.ErrorLogs] = "https://discord.com/api/webhooks/953778381252591626/dPdg0tm1RmpFpxgHarVIS9L8hhF4ySjP4Zy5snP0GOxRJ7SXb92yUE8jpmuVe7fvzEKW",
-        [WebhookType.ConsoleCommandLogs] = "https://discord.com/api/webhooks/1014474180445605941/RIHKIDqJDnUo7u_VRNpPARB_zcKIj_Ui95B4UjX7E-8jze2lLEu2iXX-krUNsCDxVSjK"
+        [WebhookType.CommandLogs] = "",
+        [WebhookType.GameLogs] = "",
+        [WebhookType.KillLogs] = "",
+        [WebhookType.ErrorLogs] = "",
+        [WebhookType.ConsoleCommandLogs] = ""
     };
 }
